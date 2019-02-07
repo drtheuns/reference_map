44dadbd480b4c2ccd8bb84ac3159b0dca9580ee7
Randall Theuns <randall.theuns@protonmail.com>
2019-02-07 01:48:02 +0100

    Initial commit; mix new

---
+++ lib/reference_map.ex
@@ -0,0 +1,18 @@
+defmodule ReferenceMap do
+  @moduledoc """
+  Documentation for ReferenceMap.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReferenceMap.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/reference_map_test.exs
@@ -0,0 +1,8 @@
+defmodule ReferenceMapTest do
+  use ExUnit.Case
+  doctest ReferenceMap
+
+  test "greets the world" do
+    assert ReferenceMap.hello() == :world
+  end
+end
